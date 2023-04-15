9306514e110d370514267675aae66548c36624c8
Yogesh Pandey <Yogisam72@users.noreply.github.com>
2023-04-15 16:57:34 +0530

    First Commit

---
+++ src/main/java/com/filer/docsys/DocSysApplication.java
@@ -0,0 +1,13 @@
+package com.filer.docsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filer/docsys/DocSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filer.docsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
